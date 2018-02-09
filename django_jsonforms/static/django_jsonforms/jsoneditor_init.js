3fa19347d72cdeae91e391696381784a5a6bdb71
Harry White <harry.white424@gmail.com>
2018-02-09 13:21:14 +1100

    Added hidden field in template, made init code set hidden to editor and disable editor on form submit

--- django_jsonforms/static/django_jsonforms/jsoneditor_init.js
+++ django_jsonforms/static/django_jsonforms/jsoneditor_init.js
@@ -1,14 +1,26 @@
 $('document').ready(function() {
-  var element = document.getElementById('editor_holder');
-  // load options from a view in here?
-  console.log(element);
-  var schema = JSON.parse(document.getElementById('editor_holder').getAttribute('schema'));
-  var options = JSON.parse(document.getElementById('editor_holder').getAttribute('options'));
-  options.schema = schema
-  var editor = new JSONEditor(element, options);
 
-  $('#submit_editor').click(function() {
-      console.log(editor.getValue());
-  })
+    $('.editor_holder').each(function() {
+        // Get the DOM Element
+        var element = $(this).get(0);
 
+        var schema = JSON.parse($(this).attr('schema'));
+        var options = JSON.parse($(this).attr('options'));
+        var name = $(this).attr('name')
+        options.schema = schema;
+        options.form_name_root = $(this).attr('name');
+        var editor = new JSONEditor(element, options);
+
+        // Check if editor is within form
+        var form = $(this).closest('form')
+        if (form) {
+            $(form).submit(function() {
+                // Set the hidden field value to the editors value
+                var hidden_identifier = 'input[name=' + name + ']';
+                $(hidden_identifier).val(JSON.stringify(editor.getValue()));
+                // Disable the editor so it's values wont be submitted
+                editor.disable();
+            })
+        }
+    });
 })
