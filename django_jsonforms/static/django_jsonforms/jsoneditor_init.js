28dc68d6205f487e648e931f58be990d1252c2cb
Harry White <harry.white424@gmail.com>
2018-02-09 13:57:36 +1100

    Made initial values rendered correctly and passed on to json editor

--- django_jsonforms/static/django_jsonforms/jsoneditor_init.js
+++ django_jsonforms/static/django_jsonforms/jsoneditor_init.js
@@ -9,6 +9,11 @@ $('document').ready(function() {
         var name = $(this).attr('name')
         options.schema = schema;
         options.form_name_root = $(this).attr('name');
+
+        // Pass initial value though to editor
+        var hidden_identifier = 'input[name=' + name + ']';
+        options.startval = JSON.parse($(hidden_identifier).val());
+
         var editor = new JSONEditor(element, options);
 
         // Check if editor is within form
@@ -16,7 +21,6 @@ $('document').ready(function() {
         if (form) {
             $(form).submit(function() {
                 // Set the hidden field value to the editors value
-                var hidden_identifier = 'input[name=' + name + ']';
                 $(hidden_identifier).val(JSON.stringify(editor.getValue()));
                 // Disable the editor so it's values wont be submitted
                 editor.disable();
