93e8a13155bf31dd03b2227225d6700fc711ca13
Harry White <harry.white424@gmail.com>
2018-02-13 09:36:18 +1100

    Fixed setup

--- setup.py
+++ setup.py
@@ -6,16 +6,16 @@ from os import path
 here = path.abspath(path.dirname(__file__))
 
 # Get the long description from the README file
-with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
     long_description = f.read
 
 setup(
     name='django-jsonforms',
     version='0.5',
     description='JSON Schema forms for Django',
+    long_description=long_description,
     url='https://github.com/Aristotle-Metadata-Enterprises/django-jsonforms',
     author='Harry White',
-    long_description=long_description,
     packages=find_packages(),
     install_requires=['jsonfield','jsonschema','django']
 )
