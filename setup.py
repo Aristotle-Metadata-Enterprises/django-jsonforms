141a6c6ffe3fdfdc18239c50d93aad31c21bfdb7
Harry White <harry.white424@gmail.com>
2018-02-21 10:41:16 +1100

    Uppdates to setup and tox config

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ here = path.abspath(path.dirname(__file__))
 __version__ = '1.0.0'
 
 # Get the long description from the README file
-with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(
