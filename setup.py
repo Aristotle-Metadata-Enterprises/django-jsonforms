1f5dd6775efbd6c7df3885dccb2a3d431daec863
Harry White <harry.white424@gmail.com>
2018-02-21 10:18:40 +1100

    verison change

--- setup.py
+++ setup.py
@@ -5,17 +5,20 @@ from os import path
 
 here = path.abspath(path.dirname(__file__))
 
+__version__ = '1.0.0'
+
 # Get the long description from the README file
 with open(path.join(here, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(
     name='django-jsonforms',
-    version='0.6',
+    version=__version__,
     description='JSON Schema forms for Django',
     long_description=long_description,
     url='https://github.com/Aristotle-Metadata-Enterprises/django-jsonforms',
     author='Harry White',
     packages=find_packages(),
-    install_requires=['jsonschema','django']
+    install_requires=['jsonschema','django'],
+    license='BSD'
 )
