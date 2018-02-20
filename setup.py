df7608a119964d177487f1068af39c3602befeba
Harry White <harry.white424@gmail.com>
2018-02-20 15:47:58 +1100

    Fixed setup.py long description

--- setup.py
+++ setup.py
@@ -7,13 +7,13 @@ here = path.abspath(path.dirname(__file__))
 
 # Get the long description from the README file
 with open(path.join(here, 'README.md'), encoding='utf-8') as f:
-    long_description = f.read
+    long_description = f.read()
 
 setup(
     name='django-jsonforms',
     version='0.6',
     description='JSON Schema forms for Django',
-    long_description='long_description',
+    long_description=long_description,
     url='https://github.com/Aristotle-Metadata-Enterprises/django-jsonforms',
     author='Harry White',
     packages=find_packages(),
