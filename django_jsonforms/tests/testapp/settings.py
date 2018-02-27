61a4b7201aa97f74a48fbd4072610d533a40c9bf
Harry White <harry.white424@gmail.com>
2018-02-27 15:18:59 +1100

    Switched to in memory database for tests

--- django_jsonforms/tests/testapp/settings.py
+++ django_jsonforms/tests/testapp/settings.py
@@ -65,7 +65,7 @@ TEMPLATES = [
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': None,
     }
 }
 
