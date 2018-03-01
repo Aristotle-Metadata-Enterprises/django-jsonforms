ef8453a5dcf7d8b8a6017d6e5f9891f511ce6726
Harry White <harry.white424@gmail.com>
2018-03-01 17:15:50 +1100

    added package data

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from os import path
 
 here = path.abspath(path.dirname(__file__))
 
-__version__ = '1.0.3'
+__version__ = '1.0.4'
 
 # Get the long description from the README file
 with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
@@ -16,6 +16,7 @@ setup(
     version=__version__,
     description='JSON Schema forms for Django',
     long_description=long_description,
+    include_package_data=True,
     url='https://github.com/Aristotle-Metadata-Enterprises/django-jsonforms',
     author='Harry White',
     packages=find_packages(),
